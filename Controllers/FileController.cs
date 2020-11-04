a84d7aceee6aed488eedc79fb2e9d569b54a3a41
JulieQiu0332 <61534881+JulieQiu0332@users.noreply.github.com>
2020-11-03 16:03:58 -0800

    test

--- Controllers/FileController.cs
+++ Controllers/FileController.cs
@@ -31,7 +31,7 @@ namespace lab02_MVC.Controllers
 
         public IActionResult Index()
         { 
-            ViewBag.Files = Directory.GetFiles( _webHostEnvironment.ContentRootPath);
+            ViewBag.Files = Directory.GetFiles("./TextFiles");
             return View();
         }
 
